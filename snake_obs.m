6f5bd16fc23c62528b6bd8f6c25f5162bae89c93
trhr <trhr@MSI>
2022-04-09 14:46:50 +0900

    complete debug

--- snake_obs.m
+++ snake_obs.m
@@ -68,33 +68,75 @@ trajectory_load=[0, 0];
 %                      snake.snake_pathlog(i, 1), load];
 % end
 for i=1:size(snake.snake_pathlog, 1)
-    %符号の決定
-    for k=1:size(obs2d, 1)
-        if obs2d(k, 4)==0
+%     %符号の決定
+%     for k=1:size(obs2d, 1)
+%         if obs2d(k, 4)==0
+%             if snake.snake_pathlog(i, 2)>0
+%                 obs2d(k, 4)=1;
+%             else
+%                 disp([snake.snake_pathlog(i, 1), snake.snake_pathlog(i, 2)])
+%                 obs2d(k, 4)=-1;
+%             end
+%         end
+%     end
+
+
+    %nearest=[flag(障害物内に入っているかの判定), obs]
+    serpen_x_=snake.snake_pathlog(i, 1);
+    serpen_y_=snake.snake_pathlog(i, 2);
+    dist=sqrt((serpen_x_-obs2d(1, 2))^2+(serpen_y_-obs2d(1, 3))^2);
+    if dist<obs2d(1, 1)
+        %符号の決定
+        if obs2d(1, 4)==0
             if snake.snake_pathlog(i, 2)>0
-                obs2d(k, 4)=1;
+                obs2d(1, 4)=1;
             else
-                disp([snake.snake_pathlog(i, 1), snake.snake_pathlog(i, 2)])
-                obs2d(k, 4)=-1;
+                obs2d(1, 4)=-1;
             end
         end
+        nearest=[1, obs2d(1,:)];
+    else
+        nearest=[0, obs2d(1,:)];
     end
-    nearest_obs=findNearestObs(obs2d, snake.snake_pathlog(i, 1), snake.snake_pathlog(i, 2));
+    min_dist=sqrt((serpen_x_-obs2d(1, 2))^2+(serpen_y_-obs2d(1, 3))^2);
+    for k=2:size(obs2d, 1)
+        dist=sqrt((serpen_x_-obs2d(k, 2))^2+(serpen_y_-obs2d(k, 3))^2);
+        if dist<min_dist
+            if dist<obs2d(k, 1)
+                %符号の決定
+                if obs2d(k, 4)==0
+                    if snake.snake_pathlog(i, 2)>0
+                        obs2d(k, 4)=1;
+                    else
+                        obs2d(k, 4)=-1;
+                    end
+                end
+                nearest=[1, obs2d(k, :)];
+            else
+                nearest=[0, obs2d(k, :)];
+            end
+            min_dist=dist;
+        end
+    end
+
+    nearest_obs=nearest;
+    %nearest_obs=findNearestObs(obs2d, snake.snake_pathlog(i, 1), snake.snake_pathlog(i, 2));
     if  nearest_obs(1, 1) == 1
         r=nearest_obs(1, 2);
         x0=nearest_obs(1, 3);
         y0=nearest_obs(1, 4);
-        %disp([x0 y0, x_path, y_path])
-        temp_y=y0+sqrt(r^2-(snake.snake_pathlog(i, 1)-x0)^2);
-%         disp(nearest_obs)
-        load=nearest_obs(1, 5)*(temp_y-snake.snake_pathlog(i, 2));
+        if nearest_obs(1, 5)==1
+            temp_y=y0+sqrt(r^2-(snake.snake_pathlog(i, 1)-x0)^2);
+        else
+            temp_y=y0-sqrt(r^2-(snake.snake_pathlog(i, 1)-x0)^2);
+        end
+        load=temp_y-snake.snake_pathlog(i, 2);
     else
         load=0;
     end
     trajectory_load=[trajectory_load; 
                      snake.snake_pathlog(i, 1), load];
 end
-disp(obs2d)
 trajectory_load=trajectory_load(1:end, :);
 
 %グラフの表示
@@ -127,26 +169,26 @@ title('load')
 
 %dtheta/dtを決定
 
-function ans_obs=findNearestObs(obs_, serpen_x_, serpen_y_)
-    %nearest=[flag(障害物内に入っているかの判定), obs]
-    dist=sqrt((serpen_x_-obs_(1, 2))^2+(serpen_y_-obs_(1, 3))^2);
-    if dist<obs_(1, 1)
-        nearest=[1, obs_(1,:)];
-    else
-        nearest=[0, obs_(1,:)];
-    end
-    min_dist=sqrt((serpen_x_-obs_(1, 2))^2+(serpen_y_-obs_(1, 3))^2);
-    for i=2:size(obs_, 1)
-        dist=sqrt((serpen_x_-obs_(i, 2))^2+(serpen_y_-obs_(i, 3))^2);
-        if dist<min_dist
-            if dist<obs_(i, 1)
-                nearest=[1, obs_(i, :)];
-            else
-                nearest=[0, obs_(i, :)];
-            end
-            min_dist=dist;
-        end
-    end
-
-    ans_obs=nearest;
-end
+% function ans_obs=findNearestObs(obs_, serpen_x_, serpen_y_)
+%     %nearest=[flag(障害物内に入っているかの判定), obs]
+%     dist=sqrt((serpen_x_-obs_(1, 2))^2+(serpen_y_-obs_(1, 3))^2);
+%     if dist<obs_(1, 1)
+%         nearest=[1, obs_(1,:)];
+%     else
+%         nearest=[0, obs_(1,:)];
+%     end
+%     min_dist=sqrt((serpen_x_-obs_(1, 2))^2+(serpen_y_-obs_(1, 3))^2);
+%     for i=2:size(obs_, 1)
+%         dist=sqrt((serpen_x_-obs_(i, 2))^2+(serpen_y_-obs_(i, 3))^2);
+%         if dist<min_dist
+%             if dist<obs_(i, 1)
+%                 nearest=[1, obs_(i, :)];
+%             else
+%                 nearest=[0, obs_(i, :)];
+%             end
+%             min_dist=dist;
+%         end
+%     end
+% 
+%     ans_obs=nearest;
+% end
