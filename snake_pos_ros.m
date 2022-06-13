9ea2f41b0547a5355728dc29945fed8b17f78167
TsuruharaKota <m1830@s.akashi.ac.jp>
2022-06-13 11:11:37 +0900

    fix

--- snake_pos_ros.m
+++ snake_pos_ros.m
@@ -1,5 +1,5 @@
 %蛇型ロボットの各パラメータ
-num_joint = 10;
+num_joint = 30;
 %length_one_jointは整数じゃないとバグる
 length_one_joint = 2.0;
 length_quarter = 5.0;
@@ -12,8 +12,9 @@ publish_command = rosmessage(matlab_pub);
 
 snake=SnakeRobot(num_joint, length_one_joint, length_quarter, alpha_yaw, alpha_pitch, dim);
 snake.changeVel(1);
-
-for i = 1:100
+r = rosrate(5);
+reset(r)
+for i = 1:1000
     snake.updateModel();
     snake.calDiscretization();
     plot(snake.snake_pathlog(:,1), snake.snake_pathlog(:,2))
@@ -24,4 +25,5 @@ for i = 1:100
         publish_command.Data(p, 1)=snake.joint_radlog(p, 1);
     end
     send(matlab_pub, publish_command);
+    waitfor(r);
 end
